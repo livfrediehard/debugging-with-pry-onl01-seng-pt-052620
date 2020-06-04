a7031d6b797e81296c61fda219e61ef584ea568d
Brian Palmer <bpalmer710@gmail.com>
2020-06-04 15:48:26 +0000

    Automatically backed up by Learn

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,7 +1,7 @@
-require 'pry'
-require './lib/pry_debugging.rb'
+#require 'pry'
+#require './lib/pry_debugging.rb'
 
-d1: def plus_two(num)
+def plus_two(num)
     num + 2
     num 
     binding.pry
